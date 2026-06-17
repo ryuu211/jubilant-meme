1c6f7d271c2043dd04cc3d5c137c469415500d31
Zakyf <122523095+ryuu211@users.noreply.github.com>
2026-06-17 14:28:18 +0700

    Fix syntax error in age count calculation

--- app_gaussian.py
+++ app_gaussian.py
@@ -9,7 +9,7 @@ data=pd.read_excel('data_ujian_akhir.xlsx', engine='openpyxl')
 st.write("Data Training",data.head(50))
 st.write("jumlah data tiap kelas: ")
 st.write(data.iloc[:,-1].value_counts())
-jumlah_usia_39= data['Age'] == 39).sum()
+jumlah_usia_39= (data['Age'] == 39).sum()
 st.write("Jumlah data Age dengan Nilai 39: ",jumlah_usia_39)
 
 x=data.iloc[:,[0,1,2,3]].values
